c3e2de10801481cb789d6f9853eaedfabbf2cf79
klinsec <39673384+klinsec@users.noreply.github.com>
2026-02-11 16:10:57 +0100

    Update 1.4.17

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import BottomNav from './components/BottomNav.js';
 import { ClockIcon, ChartIcon, ChecklistIcon, RoutineIcon } from './components/Icons.js';
 import ErrorBoundary from './components/ErrorBoundary.js';
 
-const APP_VERSION = '1.4.16';
+const APP_VERSION = '1.4.17';
 
 const CloudIconIndicator = () => {
     const { cloudStatus } = useTimeTracker();
